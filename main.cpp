fe73aae366a52ed3faa4a1a64a6c4f5429a384cb
Kamilla Youver / Archisha <95626487+archisha69@users.noreply.github.com>
2022-09-01 20:04:12 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,22 +1,29 @@
 #include <iostream>
+#include <string>
+#include <cstdlib>
+#include <ctime>
 
 using namespace std;
 
 static const char alphanum[] = "0123456789" "ABCDEFGHIJKLMNOPQRSTUVWXYZ" "abcdefghijklmnopqrstuvwxyz";
 int len = sizeof(alphanum) - 1;
 char genRandom() {
-   return alphanum[rand() % len];
+    return alphanum[rand() % len];
 }
 
 int main() {
     
-    
+    string name;
     string cwd = filesystem::current_path();
     
     int count = 0;
     
     //while (count < 200)
     while (true) {
+        srand(time(0));
+        for(int z = 0; z < 20; z++) {
+            name = genRandom();
+        }
         
     }
 }
