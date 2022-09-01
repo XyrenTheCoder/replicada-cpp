9fc4441beb1d150a2b037f63fa59b4cf248b5822
Kamilla Youver / Archisha <95626487+archisha69@users.noreply.github.com>
2022-09-01 19:59:42 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
 
 using namespace std;
+
+static const char alphanum[] = "0123456789" "ABCDEFGHIJKLMNOPQRSTUVWXYZ" "abcdefghijklmnopqrstuvwxyz";
+int len = sizeof(alphanum) - 1;
+char genRandom() {
+   return alphanum[rand() % len];
+}
+
 int main() {
     
     
